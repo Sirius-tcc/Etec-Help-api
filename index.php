a4e93c3b43ffaf332aa5b63a75d2daae160307a4
Vitor-Carmo <vitorv0071@gmail.com>
2020-11-13 00:23:49 -0300

    change the api header

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-    header("Cache-Control: no-cache, no-store, must-revalidate"); // limpa o cache
+    header("Access-Control-Allow-Credentials: true");header("Access-Control-Max-Age: 1000");
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+    header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+    header("Cache-Control: no-cache, no-store, must-revalidate");// limpa o cache
     header("Access-Control-Allow-Origin: *");
-    header('Content-Type: application/json; charset=utf-8');
+    header("Content-Type: application/json; charset=utf-8");
 
     require_once 'classes/Helper.php';
     require_once 'classes/Student.php';
